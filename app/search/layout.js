f4cb929823972eda53d1c3057e5eaf191dcaae17
Simone <simonedescode@gmail.com>
2023-12-03 19:28:38 +0000

    installed zustand + created store.js file

--- app/search/layout.js
+++ app/search/layout.js
@@ -1,12 +1,16 @@
 import Header from "./Header";
 
+
 export default function SearchLayout({ children }) {
-    return (
-        <>
-        <Header />
-        <main className="relative top-[7.5rem] pt-4 container mx-auto">
-        {children}
-        </main>
-        </>
-    )
+
+
+
+  return (
+    <>
+      <Header />
+      <main className="relative top-[7.5rem] pt-4 container mx-auto">
+      {children}
+      </main>
+    </>
+  )
 }
